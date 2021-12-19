6fbcec8234224f3ada22e1c637f09cebe91bacd1
Theanhpham11520 <panh23368@gmail.com>
2021-12-19 15:24:25 +0700

    update config

---
+++ config/common.php
@@ -0,0 +1,13 @@
+<?php
+return [
+    'select_support' => [
+        1 => "Nạp tiền",
+        2 => "Facebook",
+        3 => "VIP Facebook",
+        4 => "Instagram",
+        5 => "Tiktok",
+        6 => "Youtube",
+        7 => "Shopee",
+    ],
+]
+?>
